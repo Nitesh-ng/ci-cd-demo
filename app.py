9f1221391319cc6e767cb260487d2c33075140f1
Nitesh-ng <gaikwadnitesh015@gmail.com>
2025-10-29 03:46:28 +0530

    Test webhook trigger - changed app message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Your modified Flask app is running successfully!"
+    return "github webhook running successfully!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
